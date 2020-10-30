5509fe6e3c53ab285e068b1236c742cf8e8dc2e4
Vicente015 <expertope@gmail.com>
2020-10-30 23:41:37 +0000

    :sparkles: hey :eyes:

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  blue: '#FFCC66',
+  green: '#a4d037',
+  purple: '#ad26b4',
+  yellow: '#fecd35',
+  red: '#f05230',
+  darkgrey: '#172a45',
+  midgrey: '#738a94',
+  lightgrey: '#e6f1ff',
+  whitegrey: '#e6f1ff',
+  pink: '#fa3a57',
+  brown: '#a3821a',
+  darkmode: '#172a45',
+};
